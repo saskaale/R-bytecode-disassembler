1ede9f3c6f2e6a46371525a2cbb06f2c911e0f27
Aleš Saska <mail@alessaska.cz>
2016-10-26 13:59:24 +0200

    added origin info

--- basics.R
+++ basics.R
@@ -1,3 +1,9 @@
+######################################
+#                                    #
+#   copied from GNU R repository     #
+#                                    #
+######################################
+
 library(compiler)
 
 options(keep.source=TRUE)
