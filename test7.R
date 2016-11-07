48707c92179c7c728510a887e9b2fce452a56315
Aleš Saska <mail@alessaska.cz>
2016-11-07 23:31:12 +0100

    more tests

---
+++ test7.R
@@ -0,0 +1,14 @@
+library(compiler)
+
+options(keep.source = TRUE)
+
+
+source("basics.R")
+source("disasm.R")
+
+parsed <- compiler::cmpfun(sf)
+#getParseText(parsed, c(2,3,4,5,6))
+#lapply(attr(parsed, "srcref"), unclass)
+
+dumpDisassemble(compiler::disassemble(parsed), verbose=TRUE)
+
