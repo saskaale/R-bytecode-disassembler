df1a92509462d545e3f1088a8ceb7b64e4ca1f8d
ales_saska <mail@alessaska.cz>
2018-03-31 11:15:44 -0500

    tests

--- bctools/tests/basics.R
+++ bctools/tests/basics.R
@@ -7,9 +7,9 @@ x <- 2
 
 
 out <- getOutput(compile(quote(x + 1)))
-code <- c("GETVAR x", 
-        "LDCONST 1", 
-        "ADD", 
+code <- c("GETVAR x",
+        "LDCONST 1",
+        "ADD",
         "RETURN")
 
 
@@ -23,12 +23,12 @@ f <- function(x) x
 
 out <- getOutput( compile( quote({f(1); f(2)}) ))
 code <- c("GETFUN f",
-            "PUSHCONSTARG 1", 
-            "CALL f(1)", 
-            "POP", 
-            "GETFUN f", 
-            "PUSHCONSTARG 2", 
-            "CALL f(2)", 
+            "PUSHCONSTARG 1",
+            "CALL f(1)",
+            "POP",
+            "GETFUN f",
+            "PUSHCONSTARG 2",
+            "CALL f(2)",
             "RETURN")
 
 stopifnot(eqOut(operands(out), code))
@@ -39,15 +39,14 @@ f <- function(...) list(...)
 
 out <- getOutput(cmpfun(f))
 code <- c("BASEGUARD list(...) | $1",
-            "GETFUN list", 
-            "DODOTS", 
-            "CALL list(...)", 
-            "RETURN", 
-            "1:", 
+            "GETFUN list",
+            "DODOTS",
+            "CALL list(...)",
+            "RETURN",
+            "1:",
             "RETURN")
 
 stopifnot(eqOut(operands(out), code))
-
 
 ## simple loops
 sr <- function(x) {
@@ -104,47 +103,48 @@ switchfc <- cmpfun(switchf)
 closurefc <- cmpfun(closuref)
 
 out <- getOutput(src)
-code <- c("BASEGUARD length(x) | $1", "GETBUILTIN length", "GETVAR x", 
-        "PUSHARG", "CALLBUILTIN length(x)", "1:", "SETVAR n", "POP", 
-        "LDCONST 1", "SETVAR i", "POP", "LDCONST 0", "SETVAR s", "POP", 
-        "2:", "GETVAR i", "GETVAR n", "GT", "BRIFNOT if (i > n) break | $3", 
-        "GOTO $6", "GOTO $4", "3:", "LDNULL", "4:", "POP", "GETVAR s", 
-        "GETVAR x", "STARTSUBSET_N x[i] | $5", "GETVAR_MISSOK i", "VECSUBSET x[i]", 
-        "5:", "ADD", "SETVAR s", "POP", "GETVAR i", "LDCONST 1", "ADD", 
-        "SETVAR i", "POP", "GOTO $2", "6:", "LDNULL", "POP", "GETVAR s", 
+code <- c("BASEGUARD length(x) | $1", "GETBUILTIN length", "GETVAR x",
+        "PUSHARG", "CALLBUILTIN length(x)", "1:", "SETVAR n", "POP",
+        "LDCONST 1", "SETVAR i", "POP", "LDCONST 0", "SETVAR s", "POP",
+        "2:", "GETVAR i", "GETVAR n", "GT", "BRIFNOT if (i > n) break | $3",
+        "GOTO $6", "GOTO $4", "3:", "LDNULL", "4:", "POP", "GETVAR s",
+        "GETVAR x", "STARTSUBSET_N x[i] | $5", "GETVAR_MISSOK i", "VECSUBSET x[i]",
+        "5:", "ADD", "SETVAR s", "POP", "GETVAR i", "LDCONST 1", "ADD",
+        "SETVAR i", "POP", "GOTO $2", "6:", "LDNULL", "POP", "GETVAR s",
         "RETURN")
 stopifnot(eqOut(operands(out), code))
 
 
 out <- getOutput(swc)
-code <- c("BASEGUARD length(x) | $1", "GETBUILTIN length", "GETVAR x", 
-        "PUSHARG", "CALLBUILTIN length(x)", "1:", "SETVAR n", "POP", 
-        "LDCONST 1", "SETVAR i", "POP", "LDCONST 0", "SETVAR s", "POP", 
-        "2:", "GETVAR i", "GETVAR n", "LE", "BRIFNOT while (i <= n) { s <- s + x[i]; i <- i + 1 } | $4", 
-        "GETVAR s", "GETVAR x", "STARTSUBSET_N x[i] | $3", "GETVAR_MISSOK i", 
-        "VECSUBSET x[i]", "3:", "ADD", "SETVAR s", "POP", "GETVAR i", 
-        "LDCONST 1", "ADD", "SETVAR i", "POP", "GOTO $2", "4:", "LDNULL", 
+code <- c("BASEGUARD length(x) | $1", "GETBUILTIN length", "GETVAR x",
+        "PUSHARG", "CALLBUILTIN length(x)", "1:", "SETVAR n", "POP",
+        "LDCONST 1", "SETVAR i", "POP", "LDCONST 0", "SETVAR s", "POP",
+        "2:", "GETVAR i", "GETVAR n", "LE", "BRIFNOT while (i <= n) { s <- s + x[i]; i <- i + 1 } | $4",
+        "GETVAR s", "GETVAR x", "STARTSUBSET_N x[i] | $3", "GETVAR_MISSOK i",
+        "VECSUBSET x[i]", "3:", "ADD", "SETVAR s", "POP", "GETVAR i",
+        "LDCONST 1", "ADD", "SETVAR i", "POP", "GOTO $2", "4:", "LDNULL",
         "POP", "GETVAR s", "RETURN")
 stopifnot(eqOut(operands(out), code))
 
 
 out <- getOutput(sfc)
-code <- c("LDCONST 0", "SETVAR s", "POP", "GETVAR x", "STARTFOR y | $2", 
-        "1:", "GETVAR s", "GETVAR y", "ADD", "SETVAR s", "POP", "2:", 
+code <- c("LDCONST 0", "SETVAR s", "POP", "GETVAR x", "STARTFOR y | $2",
+        "1:", "GETVAR s", "GETVAR y", "ADD", "SETVAR s", "POP", "2:",
         "STEPFOR $1", "ENDFOR", "POP", "GETVAR s", "RETURN")
 stopifnot(eqOut(operands(out), code))
 
+
 out <- getOutput(switchfc)
-code <- c("GETVAR type", "SWITCH switch(type, mean = 1, median = 2, trimmed = 3) | c(\"mean\", \"median\", \"trimmed\", \"\") | c(11L, 14L, 17L, 8L) | c(11L, 14L, 17L, 8L)", 
-        "LDNULL", "INVISIBLE", "RETURN", "LDCONST 1", "RETURN", "LDCONST 2", 
-        "RETURN", "LDCONST 3", "RETURN")
+code <- c("GETVAR type", "SWITCH switch(type, mean = 1, median = 2, trimmed = 3) | c(\"mean\", \"median\", \"trimmed\", \"\") | $2, $3, $4, $1 | $2, $3, $4, $1",
+        "1:", "LDNULL", "INVISIBLE", "RETURN", "2:", "LDCONST 1", "RETURN",
+        "3:", "LDCONST 2", "RETURN", "4:", "LDCONST 3", "RETURN")
 stopifnot(eqOut(operands(out), code))
 
 
 out <- getOutput(closurefc)
-code <- c("MAKECLOSURE <FUNCTION>", "LDCONST 12", "SETVAR v", "POP", 
-        "MAKECLOSURE <FUNCTION>", "GETVAR v", "LDCONST 10", "MUL", "RETURN", 
-        "RETURN", "CHECKFUN", "CALL (function() { v <- 12; function() { v * 10 } })()", 
-        "SETVAR f", "POP", "GETFUN f", "CALL f()", "LDCONST 1", "ADD", 
+code <- c("MAKECLOSURE <FUNCTION>", "LDCONST 12", "SETVAR v", "POP",
+        "MAKECLOSURE <FUNCTION>", "GETVAR v", "LDCONST 10", "MUL", "RETURN",
+        "RETURN", "CHECKFUN", "CALL (function() { v <- 12; function() { v * 10 } })()",
+        "SETVAR f", "POP", "GETFUN f", "CALL f()", "LDCONST 1", "ADD",
         "RETURN")
 stopifnot(eqOut(operands(out), code))
--- bctools/tests/switch.R
+++ bctools/tests/switch.R
@@ -13,36 +13,36 @@ intSwitch <- function(x){
 }
 
 code <- c("GETVAR x",
-"SWITCH switch(x, \"FRONT\", \"BACK\") | NULL | - | $2, $3, $1",
-"1:",
-"LDNULL",
-"INVISIBLE",
-"RETURN",
-"2:",
-"LDCONST \"FRONT\"",
-"RETURN",
-"3:",
-"LDCONST \"BACK\"",
-"RETURN")
+          "SWITCH switch(x, \"FRONT\", \"BACK\") | NULL | - | $2, $3, $1",
+          "1:",
+          "LDNULL",
+          "INVISIBLE",
+          "RETURN",
+          "2:",
+          "LDCONST \"FRONT\"",
+          "RETURN",
+          "3:",
+          "LDCONST \"BACK\"",
+          "RETURN")
 
 out <- getOutput(cmpfun(strSwitch))
 stopifnot(eqOut(operands(out), code))
 
 code <- c("GETVAR x",
-"SWITCH switch(x, 0, -1, -2) | NULL | - | $2, $3, $4, $1",
-"1:",
-"LDNULL",
-"INVISIBLE",
-"RETURN",
-"2:",
-"LDCONST 0",
-"RETURN",
-"3:",
-"LDCONST -1",
-"RETURN",
-"4:",
-"LDCONST -2",
-"RETURN")
+          "SWITCH switch(x, 0, -1, -2) | NULL | - | $2, $3, $4, $1",
+          "1:",
+          "LDNULL",
+          "INVISIBLE",
+          "RETURN",
+          "2:",
+          "LDCONST 0",
+          "RETURN",
+          "3:",
+          "LDCONST -1",
+          "RETURN",
+          "4:",
+          "LDCONST -2",
+          "RETURN")
 
 
 out <- getOutput(cmpfun(intSwitch))
